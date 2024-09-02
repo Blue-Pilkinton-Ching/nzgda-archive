be356d0ffb017b6b1e0eaadc099c0f31b7efec81
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-02 15:09:33 +1200

    change

--- src/app/directory/about/page.tsx
+++ src/app/directory/about/page.tsx
@@ -1,14 +1,11 @@
 import Link from 'next/link'
-import Image from 'next/image'
-
-import back from '../../../../public/images/back.svg'
 
 const classes =
   'font-bold text-wrap sm:text-3xl text-2xl hover:*:underline hover:brightness-75 duration-100 *:mb-4 inline'
 
 export default function Page() {
   return (
-    <div className="flex justify-center flex-col items-center">
+    <div className="flex justify-center flex-col items-center text-mainred mt-12">
       <Link href={'/about/whowhatwhy'} className={classes}>
         <p className="inline-block">The Who, What, & Why</p>
         <br />
@@ -25,13 +22,6 @@ export default function Page() {
       <Link href={'/about/contact'} className={classes}>
         <p className="inline-block">Contact Us</p>
         <br />
-      </Link>
-      <br />
-      <Link
-        href={'/directory/games'}
-        className="duration-100 text-mainred *:mb-4 hover:scale-125 active:scale-95 hover:rotate-12 active:-rotate-12 "
-      >
-        <Image src={back} alt={back} className="w-14 "></Image>
       </Link>
     </div>
   )
