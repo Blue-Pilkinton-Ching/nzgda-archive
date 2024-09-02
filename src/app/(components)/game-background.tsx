019c919111c2d30a845b403803cfb9158754f697
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-02 15:06:27 +1200

    change

--- src/app/(components)/game-background.tsx
+++ src/app/(components)/game-background.tsx
@@ -5,10 +5,8 @@ import { NavBar } from './nav-bar'
 
 export default async function GameBackground({
   children,
-  bothsides = false,
 }: {
   children: React.ReactNode
-  bothsides?: boolean
 }) {
   return (
     <>
@@ -20,16 +18,14 @@ export default async function GameBackground({
       <div className="absolute right-0 w-[40%] lg:w-fit top-0 -z-10">
         <Image quality={100} src={background} alt={'background'}></Image>
       </div>
-      {bothsides ? (
-        <div className="absolute left-0 w-[40%] lg:w-fit top-0 -z-10">
-          <Image
-            quality={100}
-            src={background}
-            alt={'background'}
-            className="-scale-x-100 -translate-x-[40%] -rotate-12 -translate-y-[60%]"
-          ></Image>
-        </div>
-      ) : null}
+      <div className="absolute left-0 w-[40%] lg:w-fit top-0 -z-10">
+        <Image
+          quality={100}
+          src={background}
+          alt={'background'}
+          className="-scale-x-100 -translate-x-[40%] -rotate-12 -translate-y-[60%]"
+        ></Image>
+      </div>
       <div className="xl:p-10 sm:p-6 p-5 min-h-svh">{children}</div>
       <Footer />
     </>
